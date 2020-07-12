56557686d6bf30b8698d85a0fdae044dfeb4609d
Eric Ma <ericmajinglong@gmail.com>
2020-07-12 17:30:56 -0400

    Finished probability notebook

--- src/bayes_tutorial/solutions/probability.py
+++ src/bayes_tutorial/solutions/probability.py
@@ -38,3 +38,28 @@ def coin_data_joint_likelihood(coin_data_1, coin_data_2):
 def coin_data_joint_loglikelihood(coin_data_1, coin_data_2):
     coin = fair_coin_model()
     return (np.sum(coin.logpmf(coin_data_1)), np.sum(coin.logpmf(coin_data_2)))
+
+
+def spaces_of_p():
+    answer = """
+There are actually infinite number of possible Bernoullis that we could instantiate!
+
+Because there are an infinite set of numbers in the [0, 1] interval,
+therefore there are an infinite number of possible Bernoullis that we could instantiate.
+"""
+    return answer
+
+
+def spaces_of_data():
+    answer = """
+If you assume that there are no restrictions on the outcome,
+then there should be $2^5$ ways to configure five Bernoulli draws.
+
+More generally...
+
+First off, there's no reason why we always have to have three 1s and two 0s in five draws;
+it could have been five 1s or five 0s.
+Secondly, the order of data (though it doesn't really matter in this case)
+for three 1s and two 0s might well have been different.
+"""
+    return answer
