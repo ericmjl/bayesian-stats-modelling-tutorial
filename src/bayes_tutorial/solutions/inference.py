9a744aab38af1dcd1d601827f56053a771068ed0
Eric Ma <ericmajinglong@gmail.com>
2020-07-13 20:13:56 -0400

    Add starter solutions for Inference notebooks
    Still left todo:
    
    - Place in better prompts
    - Refactor solutions out into solutions library

--- src/bayes_tutorial/solutions/inference.py
+++ src/bayes_tutorial/solutions/inference.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from scipy.stats import beta
+from scipy.stats import beta, poisson, norm
 import numpy as np
 
 
@@ -8,3 +8,15 @@ def plot_betadist_pdf(a, b):
     x = np.linspace(0, 1, 1000)
     pdf = b.pdf(x)
     plt.plot(x, pdf)
+
+
+def coin_flip_data():
+    return np.array([1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1,])
+
+
+def car_crash_data():
+    return poisson(3).rvs(25)
+
+
+def finch_beak_data():
+    return norm(12, 1).rvs(38)
